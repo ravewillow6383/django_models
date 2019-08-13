6c841c46a47f58249531800009f97ec933dff130
Raven Robertson <ravewillow6383@gmail.com>
2019-08-13 15:56:05 -0700

    working models

--- snakes_on_a_plane/snakes_on_a_plane/settings.py
+++ snakes_on_a_plane/snakes_on_a_plane/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'snakes_on_a_plane.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'postgres',
-#         'HOST': 'localhost',
-#         'PASSWORD':'docker',
-#         'PORT': 5433
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PASSWORD':'docker',
+        'PORT': 5433
+    }
+}
 
 
 # Password validation
